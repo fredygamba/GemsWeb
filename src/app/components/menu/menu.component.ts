8cccea35f1bc740cf0c3100e8aab6cb676c98981
fredygamba <fredygamba@hotmail.es>
2020-04-22 16:17:13 -0500

    Más cambios...

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/entities/User';
 
 @Component({
   selector: 'app-menu',
@@ -6,15 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent {
-  constructor() { }
 
-  login() {
-    var usuario = <HTMLInputElement> document.getElementById("inputUsuario");
-    alert("Iniciando sesión..." + usuario);
+  public email: string;
+  public password: string;
+  public user: User;
+
+  constructor() {
+    this.user = { name: null, lastname: null, email: null, password: null };
   }
 
-  register(){
-    alert("Registrando Usuario.....");
+  login() {
+    console.log("Usuario: " + this.email + " - " + this.password);
+  }
+
+  register() {
+    if (this.validateRegister(this.user)) {
+      console.log("Registrar usuario...");
+    }
+  }
+
+  validateRegister(user: User): boolean {
+    if (user != undefined && user != null) {
+    } else if (this.user.password.length > 5) {
+    } else {
+      return true;
+    }
+    return false;
   }
 
 }
