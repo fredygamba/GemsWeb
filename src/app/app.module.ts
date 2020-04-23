1fc3158a64724ac2c1f6c4d941e558bfc0c78e24
fredygamba <fredygamba@hotmail.es>
2020-04-23 15:53:51 -0500

    cambios para routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    ProfileComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
